5de90f2cd0c282715021da0277baf01db58292b6
张起灵 <kylinuranus@gmail.com>
2020-07-16 18:48:46 +0800

    222

--- Rxswift_mvvm/Class/ViewController/HomeViewController.swift
+++ Rxswift_mvvm/Class/ViewController/HomeViewController.swift
@@ -20,7 +20,7 @@ class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         bindUI()
-        print("1111")
+        print("1112221")
 
     }
     
